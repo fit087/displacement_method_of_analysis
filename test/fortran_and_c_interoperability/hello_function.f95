37e35c7387ddedbb0a4f3ba3f5a4bcfa3af2d215
Adolf <fit087@gmail.com>
2020-03-15 20:12:12 -0300

    call Fortran From C - Part 1: Hello, World

---
+++ test/fortran_and_c_interoperability/hello_function.f95
@@ -0,0 +1,6 @@
+! hello_function.f95 file
+! gfortran -c hello_function.f95
+
+function helloWorld()
+	print *, "Hello, World from Fortran function!"
+end function helloWorld
---
+++ test/fortran_and_c_interoperability/program_hello.c
@@ -0,0 +1,17 @@
+// program_hello.c file
+// gcc -c program_hello.c
+// gfortran -o hw program_hello.o hello_function.o
+// ./hw
+
+// Calling Fortran From C - Part 1: Hello, World
+// Part 1 of 3 in a short series on how to call Fortran functions from C
+// https://youtu.be/urcy6-kXZDw
+
+void helloworld_();
+
+int main(void)
+{
+	helloworld_();
+
+	return 0;
+}
